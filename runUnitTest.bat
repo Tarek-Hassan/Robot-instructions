9ba6ca889ed01dbf6cdfd8906a7d5af299a2ed3c
Tarek Hassan <tarek446240@gmail.com>
2024-02-28 11:49:19 +0200

    add bat files

--- runUnitTest.bat
+++ runUnitTest.bat
@@ -1,4 +1,9 @@
 @echo off
-cd ./Tests
-"./vendor/bin/phpunit"
+@echo off
+
+cd "%CD%\Tests"
+
+echo "Please check unitTestResult.txt for details" && pause
+.\vendor\bin\phpunit > ..\unitTestResult.txt 2>&1 && pause
+
 cmd.exe
